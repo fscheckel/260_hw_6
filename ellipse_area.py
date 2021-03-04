0990aa3962ab2a9d4b9f1cc4a65873e2a9251e05
fscheckel <77424041+fscheckel@users.noreply.github.com>
2021-03-03 16:22:07 -1000

    Add files via upload

---
+++ ellipse_area.py
@@ -0,0 +1,20 @@
+import numpy as np
+#import 
+
+def f(x):
+    return np.pi*a*b
+
+def ellipse_area(f, a=None, b=None)
+    semi_major = a
+    semi_minor = b
+    ellipse_area = ((semi_major/x)**2 + (semi_minor/y)**2)
+    return None
+
+if __name__ == "__main__":
+    a = 2
+    b = 4
+    ellipse_area_error = []
+    xs = 
+
+dist = semi_minor - semi_major
+    r = np.linalg.norm(dist)
